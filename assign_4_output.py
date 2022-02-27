e300b79f5f8e2cf66b2fe5956450a8f951cc816f
SanidhyaSharma28 <97457815+SanidhyaSharma28@users.noreply.github.com>
2022-02-27 13:37:10 +0530

    Add files via upload

---
+++ assign_4_output.py
@@ -0,0 +1,66 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: C:\Users\sani2\Desktop\question1.py =================
+Move disk 1 from source A to destination C
+Move disk 2 from source A to destination B
+Move disk 1 from source C to destination B
+Move disk 3 from source A to destination C
+Move disk 1 from source B to destination A
+Move disk 2 from source B to destination C
+Move disk 1 from source A to destination C
+
+================= RESTART: C:\Users\sani2\Desktop\question 2.py ================
+Enter the size of pascal's triangle
+5
+Using loops
+    1 
+   1 1 
+  1 2 1 
+ 1 3 3 1 
+1 4 6 4 1 
+Using Recursions
+    1 
+   1 1 
+  1 2 1 
+ 1 3 3 1 
+1 4 6 4 1 
+
+================= RESTART: C:\Users\sani2\Desktop\question 3.py ================
+Enter first number as numerator
+12
+Enter second number as denominator
+3
+part A:
+checking if quotient and remainder is a callable value or not
+False
+False
+part B:
+remainder is 0
+part c:
+numbers which were greater than 4 are
+ [5, 6, 8, 9, 10]
+part d:
+required set is:
+ {5, 6, 8, 9, 10}
+part e:
+immutable set for previous part is: frozenset({5, 6, 8, 9, 10})
+part f:
+hash value of the maximum value from above set is : 10
+
+================= RESTART: C:\Users\sani2\Desktop\question4.py =================
+object created
+the name of the student is Sanidhya and SID is 21103044.
+object is destroyed
+
+================= RESTART: C:\Users\sani2\Desktop\question 5.py ================
+Part A
+update salary of Mehak is 7000 
+Part B
+viren's record has been deleted
+
+================= RESTART: C:\Users\sani2\Desktop\question 6.py ================
+Enter a word:draw
+Enter a new word using the exact same letters to test friendship:ward
+Does the word makes sense?(y/Y or n/N)y
+The friends pass the test
